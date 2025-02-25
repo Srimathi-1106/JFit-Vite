04c926da1b30e6816a6a3b8e1b0ef768d6157c1c
Sanjaykumar J <sanjaykumar.j@jmangroup.com>
2025-02-25 13:13:53 +0530

    Dockerfile and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
   }
 })
